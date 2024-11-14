93ec222df18071d8519603f7d4c03f6af8faee31
Šárka F. <sarka.flidrova@email.cz>
2024-11-14 09:17:35 +0100

    Content: Add responsiveness.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./style.module.less";
 import clsx from "clsx";
 
@@ -8,8 +8,16 @@ interface Props {
 }
 
 export const Layout = ({ children }: Props): JSX.Element => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const homePagePath = "/";
+
+  const isHomePage = currentPath === homePagePath;
+
   return (
-    <div className={styles.layout}>
+    <div
+      className={clsx(styles.layout, { [styles.layoutOverflow]: !isHomePage })}
+    >
       <div className={styles.langBubbles}>
         <Link to="/cs">
           <div className={clsx(styles.layoutBubble, styles.layoutBubbleCs)}>
