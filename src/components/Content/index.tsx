d7fa9ccf1176b7f4b094ff5b40213a57cc7cb632
Šárka F. <sarka.flidrova@email.cz>
2024-12-02 10:30:45 +0100

    Content: Add socials links.

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { HomeOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  InstagramOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 import sarka from "./img/profile-picture.png";
 import styles from "./style.module.less";
 import clsx from "clsx";
@@ -18,11 +22,23 @@ export const Content = ({ mainContent }: Props): JSX.Element => {
 
   return (
     <>
-      {!isHomePage ? (
-        <Link to="/">
-          <HomeOutlined className={styles.homeOutlined} />
-        </Link>
-      ) : null}
+      <div className={styles.navbar}>
+        <div>
+          {!isHomePage ? (
+            <Link to="/">
+              <HomeOutlined className={styles.icons} />
+            </Link>
+          ) : null}
+        </div>
+        <div className={styles.iconsSocial}>
+          <a href="https://www.instagram.com/wickedcottageinthewoods/">
+            <InstagramOutlined className={styles.icons} />
+          </a>
+          <a href="https://www.linkedin.com/in/sarka-flidrova/">
+            <LinkedinOutlined className={styles.icons} />
+          </a>
+        </div>
+      </div>
 
       <div className={clsx(styles.bubble, styles.bubbleMain)}>
         <div className={clsx({ [styles.content]: !isHomePage })}>
